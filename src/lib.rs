ce345a648a287ebf5ba905bffe827b98b2f0faba
Chris Cassano <chris@litprotocol.com>
2021-12-12 20:29:55 +0000

    error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-use serde_json::json;
 use worker::*;
 extern crate blsttc;
 extern crate hex;
@@ -36,49 +35,71 @@ pub async fn main(req: Request, env: Env) -> Result<Response> {
     // functionality and a `RouteContext` which you can use to  and get route parameters and
     // Environment bindings like KV Stores, Durable Objects, Secrets, and Variables.
     router
-        .get("/", |req, ctx| {
+        .get("/", |req, _| {
+            // LIT Developers: Change this to whatever URL you want to redirect to if auth works properly
+            let protected_url = "https://cdn.cloudflare.steamstatic.com/steam/apps/256843487/movie480_vp9.webm?t=1626712506";
+
+            // Do not change this.  it is hardcoded to the Lit Network Master Public Key
             let network_pubkey_string = "9971e835a1fe1a4d78e381eebbe0ddc84fde5119169db816900de796d10187f3c53d65c1202ac083d099a517f34a9b62";
             let pubkey_bytes: [u8; 48] = <[u8; 48]>::from_hex(network_pubkey_string).expect("Decoding failed");
             let pubkey = PublicKey::from_bytes(pubkey_bytes).expect("parsing pubkey failed");
 
             let url_vars: HashMap<_, _> = req.url().expect("url parsing failed").query_pairs().into_owned().collect();
-            let jwt = url_vars.get("jwt").expect("getting jwt failed");
+            
+            let no_jwt = "no_jwt".to_string();
+            let jwt = url_vars.get("jwt").unwrap_or(&no_jwt);
+            if  jwt == "no_jwt"{
+                return Response::error("JWT parameter not passed in", 400)
+            }
+            println!("JWT: {:?}", jwt);
+
             let jwt_parts = jwt.split(".").collect::<Vec<&str>>();
+            if jwt_parts.len() != 3 {
+                return Response::error("JWT does not have 3 parts", 400)
+            }
             let sig_from_jwt_base64 = jwt_parts[2];
+            let jwt_header = String::from_utf8(base64::decode_config(jwt_parts[0], base64::URL_SAFE).expect("Failed to parse JSON")).expect("Failed to convert bytes to string for jwt header");
+            let jwt_payload = String::from_utf8(base64::decode_config(jwt_parts[1], base64::URL_SAFE).expect("Failed to parse JSON")).expect("Failed to convert bytes to string for jwt payload");
             let mut sig_from_jwt: [u8; 96] = [0; 96];
-            base64::decode_config_slice(sig_from_jwt_base64, base64::URL_SAFE, &mut sig_from_jwt);
+            let decode_res = base64::decode_config_slice(sig_from_jwt_base64, base64::URL_SAFE, &mut sig_from_jwt);
+            if let Err(_) = decode_res {
+                return Response::error("Signature could not be decoded from base64", 400)
+            } 
+
+            let json_header: serde_json::Value = serde_json::from_str(&jwt_header)?;
+            let json_payload: serde_json::Value = serde_json::from_str(&jwt_payload)?;
+
+            println!("JWT header: {:?}", json_header);
+            println!("JWT payload: {:?}", json_payload);
 
             let msg = format!("{:}.{:}", jwt_parts[0], jwt_parts[1]);
+            let sig = Signature::from_bytes(sig_from_jwt);
+            if let Err(_) = sig {
+                return Response::error("Signature is bad and could not be parsed", 400)
+            } 
+            let verified = pubkey.verify(&sig.unwrap(), msg);
 
-            // let sig_str = "a8a098bd87503c6d0c8fb1bad7c4c1a9ff4555c3c05b5fdd0c20144cec4a1eddac49079785ddff2120179bf9081a1e49039cefadf9725bed946ec40270e1a82a7a1f64eb1de136873c4b7d93559e8c52282c2bf15de990de3a69c74ef4f6c7f6";
-            // let sig_bytes: [u8; 96] = <[u8; 96]>::from_hex(sig_str).expect("Decoding failed");
-            let sig = Signature::from_bytes(sig_from_jwt).expect("parsing sig failed");
-            // let msg = "eyJhbGciOiJCTFMxMi0zODEiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJMSVQiLCJzdWIiOiIweGRiZDM2MGYzMDA5N2ZiNmQ5MzhkY2M4YjdiNjI4NTRiMzYxNjBiNDUiLCJjaGFpbiI6InBvbHlnb24iLCJpYXQiOjE2MzkzMzA5MDMsImV4cCI6MTYzOTM3NDEwMywiYmFzZVVybCI6Im15LWR5bmFtaWMtY29udGVudC1zZXJ2ZXIuY29tIiwicGF0aCI6Ii9jajVuN3FraHdjODZqYmZ1YzltdWd4Iiwib3JnSWQiOiIiLCJyb2xlIjoiIiwiZXh0cmFEYXRhIjoiIn0";
-
-            let verified = pubkey.verify(&sig, msg);
-            let resp = format!("Hello from Workers! {:?} and jwt {:?} and sig {:?}", verified, jwt, sig_from_jwt);
-            // println!("{:?}",resp);
-            Response::ok(resp)
-        })
-        .post_async("/form/:field", |mut req, ctx| async move {
-            if let Some(name) = ctx.param("field") {
-                let form = req.form_data().await?;
-                match form.get(name) {
-                    Some(FormEntry::Field(value)) => {
-                        return Response::from_json(&json!({ name: value }))
-                    }
-                    Some(FormEntry::File(_)) => {
-                        return Response::error("`field` param in form shouldn't be a File", 422);
-                    }
-                    None => return Response::error("Bad Request", 400),
-                }
+            
+            if !verified{
+                return Response::error("Signature could not be verified", 401);
             }
 
-            Response::error("Bad Request", 400)
-        })
-        .get("/worker-version", |_, ctx| {
-            let version = ctx.var("WORKERS_RS_VERSION")?.to_string();
-            Response::ok(version)
+            // LIT Developers: change this to the URL you are authenticating
+            if json_payload["baseUrl"] != "my-dynamic-content-server.com" {
+                return Response::error("JWT baseUrl does not match expected value", 401);
+            }
+
+            // LIT Developers: uncomment this and set to the path you are authenticating
+            // if (json_payload["path"] != "/expectedPath") {
+            //     return Response::error("JWT baseUrl does not match expected value", 401);
+            // }
+
+            // LIT Developers: if you expect anything else in orgId, role, or extraData, make sure you set it
+            if json_payload["orgId"] != "" || json_payload["role"] != "" || json_payload["extraData"] != "" {
+                return Response::error("JWT payload has unexpected value", 401);
+            }
+
+            Response::redirect(Url::parse(protected_url)?)
         })
         .run(req, env)
         .await
